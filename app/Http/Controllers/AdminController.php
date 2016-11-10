51480944eb82f7e27b655a74648a00e0dc8e08d2
IC-SOL1\imatthew <ighotouch@gmail.com>
2016-11-10 08:11:49 +0100

    excel uploader

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\User;
 use Cloudder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\UploadedFile;
 
 class AdminController extends Controller
 {
@@ -39,23 +40,49 @@ class AdminController extends Controller
 
     public function performUploadOperation(Request $request){
       $this->validate($request, [
-          'fileToUpload' => 'required'
+          'fileToUpload' => 'required|mimes:csv,xlsx'
       ]);
-      $file = $request->file('fileToUpload')->getRealPath();
-      Cloudder::upload($file, null);
+
+      if ($request->hasFile('fileToUpload')) {
+        $destinationPath = 'uploads'; // upload path
+        $extension = $request->file('fileToUpload')->getClientOriginalExtension(); // getting image extension
+        $fileName = rand(11111,99999).'.'.$extension; // renameing image
+        $path = $request->file('fileToUpload')->move($destinationPath, $fileName); // uploading file to given path
+      $this->excelToArray($path);
+        // sending back with message
+      //  Session::flash('success', 'Upload successfully');
+      //  return Redirect::to('upload');
+      //  return redirect()->route('/')->with("suc_msg", "A ticket with ID: #$ticket->ticket_id has been opened.");
+      }
 
 
-      return    Cloudder::show(Cloudder::getPublicId(), null);
+
+
+
     }
 
 
     public function excelToArray($filePath){
-      Excel::load($filePath, function($reader) {
+      \Excel::load($filePath, function($reader) {
+// { ["surname"]=> string(7) "matthew" ["first_name"]=> string(4) "igho" ["other_name"]=> float(343) ["matric_number"]=> float(33) ["nature_of_workindustry"]=> string(3) "kjk" ["name_of_company"]=> NULL ["position_held_in_the_company"]=> NULL ["email_address"]=> NULL ["mobile_number"]=> NULL ["first_degree"]=> NULL } } } }
 
           // Getting all results
-          return $results = $reader->get();
+          $results = $reader->get();
+
+          // Loop through all sheets
+          $reader->each(function($sheet) {
+            $user = new User;
+            $user->first_name = $sheet->surname;
+            $user->last_name = $sheet->last_name;
+            $user->email = $sheet->email_address;
+            $user->matricNo = $sheet->matric_number;
+            $user->other_name = $sheet->other_name;
+            $user->sex = $sheet->sex;
+            $user->save();
 
 
+          });
+          return redirect()->route('/')->with("suc_msg", count($results). " Records successfully added!");
 
       });
     }
