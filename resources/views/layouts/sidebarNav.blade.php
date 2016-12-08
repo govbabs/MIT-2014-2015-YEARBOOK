8a7e12e72e9d2c20d989fe32eb01e1e1c680546c
Ayeni Olusegun <nsegun5@gmail.com>
2016-12-08 09:38:39 +0100

    bugs

--- resources/views/layouts/sidebarNav.blade.php
+++ resources/views/layouts/sidebarNav.blade.php
@@ -7,12 +7,12 @@
       <li class="list-group-item active"><a class="link-text-color" href="{{ url('/') }}">Profile</a></li>
 
       @if(Auth::user()->role == "admin")
-        <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="/new-post">Add Post</a></li>
-          <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="/students">View Students</a></li>
-          <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="/mass-upload">Mass Upload</a></li>
+        <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="{{ url('/new-post') }}">Add Post</a></li>
+          <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="{{ url('/students') }}">View Students</a></li>
+          <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="{{ url('/mass-upload') }}">Mass Upload</a></li>
       @endif
       <li class="list-group-item">
-        <a class="link-text-color" href="{{ route('logout') }}"><span>Logout</span></a>
+          <a class="link-text-color" href="{{ route('logout') }}"><span>Logout</span></a>
       </li>
 
     </ul>
