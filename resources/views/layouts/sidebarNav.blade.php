27bc5f4a8a1dd1340859e9db442d19948c9939b4
Ayeni Olusegun <nsegun5@gmail.com>
2016-12-08 09:34:50 +0100

    fix bugs

--- resources/views/layouts/sidebarNav.blade.php
+++ resources/views/layouts/sidebarNav.blade.php
@@ -4,14 +4,16 @@
   </div>
   <div class="panel-body list-group">
     <ul class="list-group list-group-menu">
-      <li class="list-group-item active"><a class="link-text-color" href="student-profile.blade.php">Profile</a></li>
+      <li class="list-group-item active"><a class="link-text-color" href="{{ url('/') }}">Profile</a></li>
 
       @if(Auth::user()->role == "admin")
         <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="/new-post">Add Post</a></li>
           <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="/students">View Students</a></li>
           <li class="list-group-item"><a class="link-text-color" data-toggle="modal" data-target=".yrfeature" href="/mass-upload">Mass Upload</a></li>
       @endif
-      <li class="list-group-item"><a class="link-text-color" href="login.blade.php"><span>Logout</span></a></li>
+      <li class="list-group-item">
+        <a class="link-text-color" href="{{ route('logout') }}"><span>Logout</span></a>
+      </li>
 
     </ul>
   </div>
